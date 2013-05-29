c5655b6346548a6b04025b1da9954ece0a24b0ff
Blazing Pair <blazingpair@blazingcloud.net>
2013-05-29 15:17:00 -0700

    generate rails app, add unicorn and debugger gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cub::Application.config.secret_token = 'fe7d4d800ad54faa141c68a25604f84835b33161939477632e184f72e86f0680ecaa0450c28dd7659094b0b0e34461646f01327cc5e32d00513b7363817a245f'
