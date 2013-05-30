60b95601871eafcb56adc39e3a16dd84742c9330
Jen-Mei Wu & Paul Zabelin <blazingpair+jen-mei+paul@blazingcloud.net>
2013-05-30 14:25:16 -0700

    refactor assert not logged method

--- features/step_definitions/cub_steps.rb
+++ features/step_definitions/cub_steps.rb
@@ -1,8 +1,17 @@
+def assert_not_logged_in
+  visit "/"
+  page.has_content?("connect").must_equal(true)
+  current_path.must_equal("/")
+end
+
 Given(/^I visit the (.*) page$/) do |page|
   visit(path_for(page))
 end
+
 When(/^I am not logged in$/) do
+  assert_not_logged_in
 end
+
 Then(/^I should see the '(.*)' button$/) do |button|
   page.has_content?(button).must_equal(true)
 end
@@ -11,4 +20,9 @@ Given(/^There is no user with email "([^"]*)"$/) do |email|
 end
 When(/^I sign up for an account with my LinkedIn profile \("([^"]*)"\)$/) do |email|
   pending
+end
+
+When(/^I logout$/) do
+  visit "/logout"
+  assert_not_logged_in
 end
