af98506cfdb4d6f1ed991abfbcb92a39aedcdcb8
Jen-Mei Wu & Paul Zabelin <blazingpair+jen-mei+paul@blazingcloud.net>
2013-06-03 15:16:41 -0700

    style table to be max 800px change factory for session

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -1,9 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 FactoryGirl.define do
   sequence(:session_name) { |n| "session \##{n}" }
+  sequence(:room) { |n| "room \##{n}" }
   factory(:session) do
     name { generate(:session_name) }
     start_time "June 10, 2013 2:30pm".to_datetime
-    location "San Francisco, CA, USA"
+    location { generate(:room) }
   end
 end
