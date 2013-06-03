59ddaa8901cb6a6aa44b5189301d8aa4e12aa6a8
Jen-Mei Wu & Paul Zabelin <blazingpair+jen-mei+paul@blazingcloud.net>
2013-06-03 13:36:13 -0700

    show list of session for an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
+    @sessions = @event.sessions
 
     respond_to do |format|
       format.html # show.html.erb
