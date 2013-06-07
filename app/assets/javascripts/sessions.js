0ccd653926705dc110cac01f6daff1e0e05421a2
Paul Zabelin <blazingpair+paul@blazingcloud.net>
2013-06-07 15:36:40 -0700

    skip firepad instantiation when there is no firepad element

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   var firepadRef = new Firebase('http://cubnotes.firebaseIO.com/' + window.location.pathname);
+  var firepadPlace = document.getElementById('firepad');
+  if (firepadPlace == null) {
+    return;
+  }
   var codeMirror = CodeMirror(document.getElementById('firepad'), { lineWrapping: true });
   var firepad = Firepad.fromCodeMirror(
     firepadRef,
